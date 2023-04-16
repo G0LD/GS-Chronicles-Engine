067f95d1699c34ce2a70d80153e7eb84588bcbeb
Ruki Studios <berni.romero.43@gmail.com>
2023-04-16 00:46:17 -0600

    scripts update

--- assembly/overworld_scripts/LeagueTitleDefense.s
+++ assembly/overworld_scripts/LeagueTitleDefense.s
@@ -5,8 +5,8 @@
 .include "../xse_defines.s"
 .include "../asm_defines.s"
 
-#define FLAG_SCALE_TRAINER_LEVELS 0x140E
-#define VAR_STARTER_CHOSEN_SILVER 0x4060
+.equ FLAG_SCALE_TRAINER_LEVELS, 0x140E
+.equ VAR_STARTER_CHOSEN_SILVER, 0x4060
 	
 	@---------------
 EventScript_LeagueTitleDefense:
@@ -14,32 +14,38 @@ EventScript_LeagueTitleDefense:
     hidesprite 0x1
     hidesprite 0x6
     hidesprite 0x7
+    getplayerpos 0x8008 0x8009
+    compare 0x8008 0x8
+    if 0x1 _call EventScript_LeagueTitleDefense_StepRight
+    compare 0x8008 0xA
+    if 0x1 _call EventScript_LeagueTitleDefense_StepLeft
     applymovement PLAYER EventScript_LeagueTitleDefense_Move1
     waitmovement 0x0
-    random 0x6
-    compare LASTRESULT 0x5
-    if 0x1 _goto EventScript_LeagueTitleDefense_ProfElm
     msgbox 0x8C5508A 0xA
     msgbox gText_LeagueTitleDefense_ElmSpeech1 MSG_NORMAL
     callstd 0xB
     applymovement PLAYER EventScript_LeagueTitleDefense_Move2
     waitmovement 0x0
     pause 0x32
+    random 0x6
+    compare LASTRESULT 0x5
+    if 0x1 _goto EventScript_LeagueTitleDefense_ProfElm
     compare LASTRESULT 0x0
     if 0x1 _goto EventScript_LeagueTitleDefense_Lorelei
     compare LASTRESULT 0x1
     if 0x1 _goto EventScript_LeagueTitleDefense_Joey
     compare LASTRESULT 0x2
-    if 0x1 _goto EventScript_LeagueTitleDefense_Sayo
+    if 0x1 _goto EventScript_LeagueTitleDefense_Naoko
     compare LASTRESULT 0x3
     if 0x1 _goto EventScript_LeagueTitleDefense_Clair
     compare LASTRESULT 0x4
     if 0x1 _goto EventScript_LeagueTitleDefense_Silver
+    goto EventScript_LeagueTitleDefense_ProfElm
     end
 
 EventScript_LeagueTitleDefense_Lorelei:
-    showsprite 0xFE
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move3
+    showsprite 0xB
+    applymovement 0xB EventScript_LeagueTitleDefense_Move3
     waitmovement 0x0
     msgbox 0x8C3DFE1 0xA
     msgbox gText_LeagueTitleDefense_LoreleiSpeech1 MSG_NORMAL
@@ -50,15 +56,15 @@ EventScript_LeagueTitleDefense_Lorelei:
     msgbox 0x8C3DFE1 0xA
     msgbox gText_LeagueTitleDefense_LoreleiSpeech3 MSG_NORMAL
     callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
+    applymovement 0xB EventScript_LeagueTitleDefense_Move4
     waitmovement 0x0
-    hidesprite 0xFE
+    hidesprite 0xB
     goto EventScript_LeagueTitleDefense_Ending
     end
 
 EventScript_LeagueTitleDefense_Joey:
-    showsprite 0xFE
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move3
+    showsprite 0x8
+    applymovement 0x8 EventScript_LeagueTitleDefense_Move3
     waitmovement 0x0
     msgbox gText_LeagueTitleDefense_JoeyName 0xA
     msgbox gText_LeagueTitleDefense_JoeySpeech1 MSG_NORMAL
@@ -69,34 +75,34 @@ EventScript_LeagueTitleDefense_Joey:
     msgbox 0x8C3DFE1 0xA
     msgbox gText_LeagueTitleDefense_JoeySpeech3 MSG_NORMAL
     callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
+    applymovement 0x8 EventScript_LeagueTitleDefense_Move4
     waitmovement 0x0
-    hidesprite 0xFE
+    hidesprite 0x8
     goto EventScript_LeagueTitleDefense_Ending
     end
 
-EventScript_LeagueTitleDefense_Sayo:
-    showsprite 0xFE
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move3
+EventScript_LeagueTitleDefense_Naoko:
+    showsprite 0x9
+    applymovement 0x9 EventScript_LeagueTitleDefense_Move3
     waitmovement 0x0
     msgbox 0x8C4D19D 0xA
-    msgbox gText_LeagueTitleDefense_SayoSpeech1 MSG_NORMAL
+    msgbox gText_LeagueTitleDefense_NaokoSpeech1 MSG_NORMAL
     callstd 0xB
     setflag FLAG_SCALE_TRAINER_LEVELS
-    trainerbattle3 0x3 0x16D 0x200 gText_LeagueTitleDefense_SayoSpeech2
+    trainerbattle3 0x3 0x16D 0x200 gText_LeagueTitleDefense_NaokoSpeech2
     clearflag FLAG_SCALE_TRAINER_LEVELS
     msgbox 0x8C4D19D 0xA
-    msgbox gText_LeagueTitleDefense_SayoSpeech3 MSG_NORMAL
+    msgbox gText_LeagueTitleDefense_NaokoSpeech3 MSG_NORMAL
     callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
+    applymovement 0x9 EventScript_LeagueTitleDefense_Move4
     waitmovement 0x0
-    hidesprite 0xFE
+    hidesprite 0x9
     goto EventScript_LeagueTitleDefense_Ending
     end
 
 EventScript_LeagueTitleDefense_Clair:
-    showsprite 0xFE
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move3
+    showsprite 0xA
+    applymovement 0xA EventScript_LeagueTitleDefense_Move3
     waitmovement 0x0
     msgbox 0x8C4A5DB 0xA
     msgbox gText_LeagueTitleDefense_ClairSpeech1 MSG_NORMAL
@@ -107,15 +113,15 @@ EventScript_LeagueTitleDefense_Clair:
     msgbox 0x8C4A5DB 0xA
     msgbox gText_LeagueTitleDefense_ClairSpeech3 MSG_NORMAL
     callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
+    applymovement 0xA EventScript_LeagueTitleDefense_Move4
     waitmovement 0x0
-    hidesprite 0xFE
+    hidesprite 0xA
     goto EventScript_LeagueTitleDefense_Ending
     end
 
 EventScript_LeagueTitleDefense_Silver:
-    showsprite 0xFE
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move3
+    showsprite 0x2
+    applymovement 0x2 EventScript_LeagueTitleDefense_Move3
     waitmovement 0x0
     msgbox 0x8C09516 0xA
     msgbox gText_LeagueTitleDefense_SilverSpeech1 MSG_NORMAL
@@ -131,9 +137,9 @@ EventScript_LeagueTitleDefense_Silver:
     msgbox 0x8C09516 0xA
     msgbox gText_LeagueTitleDefense_SilverSpeech3 MSG_NORMAL
     callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
+    applymovement 0x2 EventScript_LeagueTitleDefense_Move4
     waitmovement 0x0
-    hidesprite 0xFE
+    hidesprite 0x2
     goto EventScript_LeagueTitleDefense_Ending
     end
 
@@ -150,8 +156,9 @@ EventScript_LeagueTitleDefense_Silver_Chikorita:
     return
 
 EventScript_LeagueTitleDefense_ProfElm:
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move5
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move5
     waitmovement 0x0
+    spriteface 0x5 UP
     msgbox 0x8C05560 0xA
     msgbox gText_LeagueTitleDefense_ProfElmSpeech1 MSG_NORMAL
     callstd 0xB
@@ -164,22 +171,158 @@ EventScript_LeagueTitleDefense_ProfElm:
     if 0x1 _call EventScript_LeagueTitleDefense_ProfElm_Cyndaquil
     clearflag FLAG_SCALE_TRAINER_LEVELS
     msgbox 0x8C05560 0xA
-    msgbox gText_LeagueTitleDefense_ProfElmSpeech3 MSG_NORMAL
-    callstd 0xB
-    applymovement 0xFE EventScript_LeagueTitleDefense_Move4
-    waitmovement 0x0
-    hidesprite 0xFE
-    goto EventScript_LeagueTitleDefense_Ending
+    msgbox gText_LeagueTitleDefense_ProfElmSpeech3 MSG_KEEPOPEN
+    goto EventScript_LeagueTitleDefense_EndingElm
     end
 
 EventScript_LeagueTitleDefense_ProfElm_Totodile:
-    trainerbattle3 0x3 0x171 0x200 gText_LeagueTitleDefense_SilverSpeech2
+    trainerbattle3 0x3 0x171 0x200 gText_LeagueTitleDefense_ProfElmSpeech2
     return
 
 EventScript_LeagueTitleDefense_ProfElm_Chikorita:
-    trainerbattle3 0x3 0x172 0x200 gText_LeagueTitleDefense_SilverSpeech2
+    trainerbattle3 0x3 0x172 0x200 gText_LeagueTitleDefense_ProfElmSpeech2
     return
 
 EventScript_LeagueTitleDefense_ProfElm_Cyndaquil:
-    trainerbattle3 0x3 0x170 0x200 gText_LeagueTitleDefense_SilverSpeech2
+    trainerbattle3 0x3 0x170 0x200 gText_LeagueTitleDefense_ProfElmSpeech2
     return
+
+EventScript_LeagueTitleDefense_Ending:
+    spriteface PLAYER UP
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move2
+    waitmovement 0x0
+    msgbox 0x8C5508A 0xA
+    msgbox gText_LeagueTitleDefense_ElmSpeech2 MSG_NORMAL
+    callstd 0xB
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move6
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move6
+    waitmovement 0x0
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move7
+    hidesprite 0x5
+    sound 0x9
+    pause 0x16
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move8
+    warp 0x1 0x50 0xFF 0x5 0x10
+    end
+
+EventScript_LeagueTitleDefense_EndingElm:
+    msgbox gText_LeagueTitleDefense_ElmSpeech2 MSG_NORMAL
+    callstd 0xB
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move9
+    waitmovement 0x0
+    spriteface 0x5 DOWN
+    spriteface PLAYER UP
+    pause 0xF
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move6
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move6
+    waitmovement 0x0
+    applymovement 0x5 EventScript_LeagueTitleDefense_Move7
+    hidesprite 0x5
+    sound 0x9
+    pause 0x16
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move8
+    warp 0x1 0x50 0xFF 0x5 0x10
+    end
+
+EventScript_LeagueTitleDefense_StepRight:
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move10
+    waitmovement 0x0
+    return
+
+EventScript_LeagueTitleDefense_StepLeft:
+    applymovement PLAYER EventScript_LeagueTitleDefense_Move11
+    waitmovement 0x0
+    return
+
+EventScript_LeagueTitleDefense_Move1:
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move2:
+.byte walk_down
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move3:
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move4:
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move5:
+.byte walk_left
+.byte walk_down
+.byte walk_down
+.byte walk_down
+.byte walk_right
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move6:
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte walk_up
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move7:
+.byte walk_up
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move8:
+.byte walk_up
+.byte walk_up
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move9:
+.byte walk_left
+.byte walk_up
+.byte walk_up
+.byte walk_right
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move10:
+.byte walk_right
+.byte end_m
+
+EventScript_LeagueTitleDefense_Move11:
+.byte walk_left
+.byte end_m
